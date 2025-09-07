593d1cbad985f26f2685a96478c0ecf2ef31a8f3
PhilippePerret <philippe.perret@yahoo.fr>
2025-09-07 10:47:25 +0200

    Implémentation de AbsNoteType pour l'analyse uniquement

--- src/classes/Slice.ts
+++ src/classes/Slice.ts
@@ -1,20 +1,19 @@
 import { MeasureType } from "./Measure";
-import { Note } from "./Note";
+import { AbsNoteType } from "./Note";
 
 export interface SliceType {
   measure: MeasureType;
   indice: number;
-  notes: Note[];
+  notes: AbsNoteType[];
 }
 
 export class Slice implements SliceType {
   measure: MeasureType;
   indice: number;
-  notes: Note[];
+  notes: AbsNoteType[];
 
   constructor(
     data: SliceType,
-    params: {[x: string]: any}
   ){
     for (let prop in data) { this[prop] = data[prop]; }
   }
